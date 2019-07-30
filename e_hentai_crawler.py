5117540ab6d01f34bc418dce2a326cb087a93a72
ches4117 <ches4117@gmail.com>
2019-07-30 23:17:04 +0800

    Fix url windows file new line %0A bug

--- e_hentai_crawler.py
+++ e_hentai_crawler.py
@@ -2,10 +2,27 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import re
 import urllib
+import requests
 
 my_file = Path("./href_list.json")
 if my_file.is_file():
-    print("!!!")
+    file = open('./href_list.json', 'r',
+                encoding='utf-8', errors='ignore')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+        'Accept-Language': 'zh-CN,zh;q=0.8'}
+    for url in file:
+        code = int(url.split('/')[4])
+        res = requests.post(
+            url.rstrip(), headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+        img_list = soup.find_all(
+            'a', href=re.compile("https://e-hentai.org/s/"))
+        for link in img_list:
+            img_url = link.get('href')
+            print(img_url)
+        break
+    file.close()
 else:
     file = open('./bookmarks_2019_7_28.html', 'r',
                 encoding='utf-8', errors='ignore')
@@ -23,4 +40,4 @@ else:
 
     file = open('./href_list.json', 'w', encoding='utf-8', errors='ignore')
     for href in href_list:
-        file.write(href + '\n')
+        file.write(href + '\r\n')
